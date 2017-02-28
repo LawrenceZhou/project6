9dafcaa14a72df45de429acdedac1d848696aaed
LawrenceZhou <hmskyijun@gmail.com>
2017-02-27 23:44:14 -0800

    1.3

--- webServer.js
+++ webServer.js
@@ -225,7 +225,7 @@ app.get('/photosOfUser/:id', function (request, response) {
                 // Query returned an error.  We pass it back to the browser with an Internal Service
                 // Error (500) error code.
                 console.error('Doing /photosOfUser/:id error:', err);
-                response.status(500).send(JSON.stringify(err));
+                response.status(400).send(JSON.stringify(err));
                 return;
             }
             if (photo === null) {
