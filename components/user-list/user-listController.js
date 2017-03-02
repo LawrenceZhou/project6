9c2849ace53e4218d21b8ce27c9893104992dc4e
LawrenceZhou <hmskyijun@gmail.com>
2017-03-01 16:59:04 -0800

    2.2

--- components/user-list/user-listController.js
+++ components/user-list/user-listController.js
@@ -1,15 +1,21 @@
 'use strict';
 
-cs142App.controller('UserListController', ['$scope',
-    function ($scope) {
+cs142App.controller('UserListController', ['$scope', '$resource',
+    function ($scope, $resource) {
         $scope.main.title = 'Users';
         //$scope.nameList = window.cs142models.userListModel();
-        $scope.FetchModel("http://localhost:3000/user/list", function(model){
+        /*$scope.FetchModel("http://localhost:3000/user/list", function(model){
             var object = JSON.parse(model);
             $scope.$apply(function () {
             // Put your code that updates any $scope variables here
             $scope.nameList = object;
             });
+        });*/
+
+        var userList = $resource('http://localhost:3000/user/list', {}, {'save': {method: 'GET'}});
+        var object = userList.get({}, function() {
+            object.$save();
+            $scope.nameList = object;
         });
 
         //console.log('window.cs142models.userListModel()', window.cs142models.userListModel());
