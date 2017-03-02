467484fbdc845ae81f41c06b3a066c3d19451902
LawrenceZhou <hmskyijun@gmail.com>
2017-03-01 16:50:07 -0800

    2.1

--- mainController.js
+++ mainController.js
@@ -59,9 +59,10 @@ cs142App.controller('MainController', ['$scope', '$resource',
         var version = $resource('http://localhost:3000/test/:param', {param: 'info'}, {'get': {method: 'GET'}, save: {method: 'GET'}});
         var object = version.get({param: 'info'}, function() {
             object.$save();
-        });
-        console.log(object);
+            console.log(object);
         $scope.main.version = JSON.parse(JSON.stringify(object)).__v;
+        });
+        
 
 
         /*$scope.FetchModel("http://localhost:3000/test/info", function(model){
