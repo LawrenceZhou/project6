ca7b330c7e509904378dc41a3c642e7564c3cd32
LawrenceZhou <hmskyijun@gmail.com>
2017-03-01 17:23:34 -0800

    2.4.1

--- components/user-photos/user-photosController.js
+++ components/user-photos/user-photosController.js
@@ -1,7 +1,7 @@
 'use strict';
 
-cs142App.controller('UserPhotosController', ['$scope', '$routeParams',
-  function($scope, $routeParams) {
+cs142App.controller('UserPhotosController', ['$scope', '$routeParams', '$resource',
+  function($scope, $routeParams, $resource) {
     /*
      * Since the route is specified as '/photos/:userId' in $routeProvider config the
      * $routeParams  should have the userId property set with the path from the URL.
@@ -17,7 +17,7 @@ cs142App.controller('UserPhotosController', ['$scope', '$routeParams',
     });
 
     //$scope.photos = window.cs142models.photoOfUserModel(userId);
-    $scope.FetchModel("http://localhost:3000/user/" + userId, function(model){
+    /*$scope.FetchModel("http://localhost:3000/user/" + userId, function(model){
         var object = JSON.parse(model);
         $scope.$apply(function () {
             // Put your code that updates any $scope variables here
@@ -27,7 +27,16 @@ cs142App.controller('UserPhotosController', ['$scope', '$routeParams',
             $scope.main.toolBar = "Photos of " + firstName + " " + lastName;
             $scope.user.name = firstName + " " + lastName;
         });
-    });
+    });*/
+
+    var user = $resource('http://localhost:3000/user/:id', {}, {});
+        var object = user.get({id: userId}, function() {
+            $scope.user = object;
+            var firstName = $scope.user.first_name;
+            var lastName = $scope.user.last_name;
+            $scope.main.toolBar = firstName + " " + lastName;
+            $scope.user.name = firstName + " " + lastName;
+        });
 
     //var firstName = $scope.user.first_name;
     //var lastName = $scope.user.last_name;
