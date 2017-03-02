8006185ed7ab054df94415cc3330be93f9b25de1
LawrenceZhou <hmskyijun@gmail.com>
2017-03-01 17:08:44 -0800

    2.2

--- components/user-list/user-listController.js
+++ components/user-list/user-listController.js
@@ -14,7 +14,7 @@ cs142App.controller('UserListController', ['$scope', '$resource',
 
         var userList = $resource('http://localhost:3000/user/list', {}, {'query': {method: 'GET', isArray : true}, 'save': {method: 'GET'}});
         var object = userList.query({}, function() {
-            object.$save();
+            //object.$save();
             $scope.nameList = object;
         });
 
